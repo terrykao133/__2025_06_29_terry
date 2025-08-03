be67aca74817f7a507a2ba7f51bf2e86e6b00428
terrykao133 <yamada490@gmail.com>
2025-08-03 02:41:58 +0000

    新增查詢全省各站點2022年進站總人數的SQL語句

--- lesson9/lesson9_1.sql
+++ lesson9/lesson9_1.sql
@@ -5,3 +5,17 @@ FROM "台鐵車站資訊"
 SELECT count(name) AS "台北車站數"
 FROM "台鐵車站資訊"
 WHERE "stationAddrTw" LIKE '%臺北%';
+
+SELECT *
+FROM "每日各站進出站人數" LEFT JOIN "台鐵車站資訊" ON "每日各站進出站人數"."車站代碼" =  "台鐵車站資訊"."stationCode"
+WHERE "stationName" = '基隆'
+
+/*
+ * 全省各站點2022年進站總人數
+ */
+
+SELECT "name" AS 站名,COUNT("name") AS 筆數,AVG("進站人數") AS "進站人數"
+FROM "每日各站進出站人數" LEFT JOIN "台鐵車站資訊" ON "車站代碼" = "stationCode"
+WHERE "日期" BETWEEN '2022-01-01' AND '2022-12-31'
+GROUP BY "name"
+
